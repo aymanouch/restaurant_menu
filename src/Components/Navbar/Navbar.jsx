50d0588fe6f219017d058ab68c1c32ebb38d34c3
ayman <aymanouhedih1997@gmail.com>
2023-05-23 09:20:19 +0100

    add logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <div id='navbar'>
         <ul>
-            <li className="logo"><a href="/">logo</a></li>
+            <li className="logo"><a href="/"><img atl="cafe" src='./my-cafe.png'/></a></li>
             <li><BiMenuAltLeft size="1.5rem"/></li>
         </ul>
     </div>
